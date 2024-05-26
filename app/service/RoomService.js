c4145e8349d403df1909bb2090d3b31e8cc48d6a
tranduytruong12 <151857423+tranduytruong12@users.noreply.github.com>
2024-05-26 18:50:37 +0700

    Update RoomService.js

--- app/service/RoomService.js
+++ app/service/RoomService.js
@@ -13,9 +13,14 @@ const getAllDevices = async (room_id) => {
 
 const getAllEvents = async (room_id) => {
   try {
-    const currentDate = new Date();
-    const events = await Calendar.find({ room: room_id, date: { $gt: currentDate } });
-    return events;
+    const currentDate = new Date().toISOString();
+    const currentDateString = currentDate.split('T')[0];
+    const events = await Calendar.find({ room: room_id });
+    const filteredEvents = events.filter(event => {
+            const eventDateString = event.date.split('T')[0]; 
+            return eventDateString > currentDateString;
+        });
+    return filteredEvents;
   } catch (error) {
     console.error("Error fetching devices: ", error);
     throw error;
