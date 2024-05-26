b3394cff70ce32e5d3236a49b2f87a9bb3d0c28f
Tien Tran <82021776+tient2002vn@users.noreply.github.com>
2024-05-26 19:18:29 +0700

    Update BarChart.jsx

--- DADN-UI/src/components/BarChart.jsx
+++ DADN-UI/src/components/BarChart.jsx
@@ -1,12 +1,69 @@
 import { useTheme } from "@mui/material";
 import { ResponsiveBar } from "@nivo/bar";
 import { tokens } from "../theme";
-import { mockBarData as data } from "../data/mockData";
+import { useEffect, useState, useCallback } from "react";
+import axios from 'axios';
 
 const BarChart = ({ isDashboard = false }) => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
-
+  const [data, setData] = useState([]);
+  // formatData fn
+  const formatData = useCallback((data) => {
+    if (!Array.isArray(data)) {
+      return [];
+    }
+    let mockBarData = [];
+    data.forEach(item => {
+      let buildingIndex = mockBarData.findIndex(building => building.building === item.building_id);
+      if (buildingIndex !== -1) {
+        if (mockBarData[buildingIndex][item.floor_id]) {
+          mockBarData[buildingIndex][item.floor_id] += item.power;
+        } else {
+          mockBarData[buildingIndex][item.floor_id] = item.power;
+          mockBarData[buildingIndex][item.floor_id + 'Color'] = getColor(item.floor_id);
+        }
+      } else {
+        mockBarData.push({
+          building: item.building_id,
+          [item.floor_id]: item.power,
+          [item.floor_id + 'Color']: getColor(item.floor_id)
+        });
+      }
+    });
+    return Array.from(mockBarData.values());
+  }, [])
+  const getColor = (floor) => {
+    switch (floor) {
+      case "T1":
+        return "hsl(72, 70%, 50%)";
+      case "T2":
+        return "hsl(96, 70%, 50%)";
+      case "T3":
+        return "hsl(106, 70%, 50%)";
+      case "T4":
+        return "hsl(9, 70%, 50%)";
+      case "T5":
+        return "hsl(326, 70%, 50%)";
+      case "T6":
+        return "hsl(256, 70%, 50%)";
+      default:
+        return "hsl(300, 70%, 50%)"
+    }
+  };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3001/home');
+        const data = response.data;
+        const dataFormated = formatData(data);
+        setData(dataFormated);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, [formatData]);
   return (
     <ResponsiveBar
       data={data}
@@ -40,7 +97,7 @@ const BarChart = ({ isDashboard = false }) => {
         },
       }}
       keys={["T1", "T2", "T3", "T4", "T5", "T6"]}
-      indexBy="country"
+      indexBy="building"
       margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
       padding={0.3}
       valueScale={{ type: "linear" }}
@@ -84,7 +141,7 @@ const BarChart = ({ isDashboard = false }) => {
         tickSize: 5,
         tickPadding: 5,
         tickRotation: 0,
-        legend: isDashboard ? undefined : "Rating", // changed
+        legend: isDashboard ? undefined : "Power", // changed
         legendPosition: "middle",
         legendOffset: -40,
       }}
@@ -121,7 +178,7 @@ const BarChart = ({ isDashboard = false }) => {
       ]}
       role="application"
       barAriaLabel={function (e) {
-        return e.id + ": " + e.formattedValue + " in country: " + e.indexValue;
+        return e.id + ": " + e.formattedValue + " in building: " + e.indexValue;
       }}
     />
   );
